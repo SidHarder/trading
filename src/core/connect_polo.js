7d8e48ff4b2659df18722781aa573d11c126f590
SidHarder <softwarenavigator@gmail.com>
2017-12-11 16:44:50 -0700

    rearanging things

---
+++ src/core/connect_polo.js
@@ -0,0 +1,26 @@
+var autobahn = require('autobahn')
+var wsuri = "wss://api.poloniex.com"
+var connection = new autobahn.Connection({
+  url: wsuri,
+  realm: "realm1"
+})
+
+connection.onopen = function (session) {
+	function marketEvent (args,kwargs) {}
+		console.log(args)
+	}
+	function tickerEvent (args,kwargs) {
+		console.log(args)
+	}
+	function trollboxEvent (args,kwargs) {
+		console.log(args)
+	}
+	session.subscribe('BTC_USDT', marketEvent)
+  console.log('opening')
+}
+
+connection.onclose = function () {
+  console.log("Websocket connection closed")
+}
+
+connection.open()
--- src/core/store-data.js
+++ src/core/store-data.js
@@ -2,12 +2,12 @@ var level = require('level')
 var request = require('request')
 var JSONStream = require("JSONStream")
 var es = require('event-stream')
-var db = level('./mydb')
 var _ = require('underscore')
 
-//https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&end=9999999999&period=14400&start=1405699200
+var url = 'https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&end=9999999999&period=14400&start=1405699200'
+var db = level('leveldb')
 
-request({ url: 'https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&end=9999999999&period=14400&start=1405699200'})
+request({ url: url})
   .pipe(JSONStream.parse())
   .pipe(es.mapSync(function (data) {
     _.each(data, function (item) {
